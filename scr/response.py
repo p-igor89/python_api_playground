5a8673f443fafd17be7672fe4fd913e7cd90cdd6
newfire <bitbucket>
2023-05-31 12:47:19 -0300

    add logging

--- scr/response.py
+++ scr/response.py
@@ -1,6 +1,11 @@
+import logging
+
+
 class AssertableResponse(object):
 
     def __init__(self, response):
+        logging.info("Request \n url={} \n body={}".format(response.request.url, response.request.body))
+        logging.info("Response \n status={} \n body={}".format(response.status_code, response.text))
         self.response = response
 
     def status_code(self, code):
